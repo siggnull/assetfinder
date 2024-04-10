e353c532ad1fc697e2f6a48dffd0c6a824bf75ff
Anatoly Ivakin <siggnull@gmail.com>
2024-04-10 19:05:44 +0300

    Remove unnecessary command line parsing configuration

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ import { program } from 'commander'
 
 program
   .version('0.0.1', '-v, --version') // duplicate of the version in package.json
-  .usage('[OPTIONS]...')
   .requiredOption('-a, --address <address>', 'The address to search for owned tokens.')
   .requiredOption('-k, --key <key>', 'Moralis API key.')
-  .allowExcessArguments(false)
   .parse(process.argv)
 
-console.log(program.args)
 console.log(program.opts())
