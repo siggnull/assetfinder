ed65f4b65d0ed277a6632e1167a9a5ae72795e5e
Anatoly Ivakin <siggnull@gmail.com>
2024-04-10 19:42:08 +0300

    Show native balance in the Ethereum network for the supplied address

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { program } from 'commander'
 import { default as moralis } from 'moralis'
+import { EvmChain } from '@moralisweb3/common-evm-utils'
 
 program
   .version('0.0.1', '-v, --version') // duplicate of the version in package.json
@@ -7,4 +8,15 @@ program
   .requiredOption('-k, --key <key>', 'Moralis API key.')
   .parse(process.argv)
 
-console.log(program.opts())
+const options = program.opts()
+
+await moralis.start({
+  apiKey: options.key
+})
+
+const balance = await moralis.EvmApi.balance.getNativeBalance({
+    address: options.address,
+    chain: EvmChain.ETHEREUM,
+})
+
+console.log(balance.jsonResponse)
