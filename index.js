3eb157c42ae24ef73f6068b171062575c3243f95
Anatoly Ivakin <siggnull@gmail.com>
2024-04-10 12:02:00 +0300

    Install commander.js and parse command line parameters

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const address = process.argv[2];
+import { program } from 'commander'
 
-console.log('Address: ' + address)
+program
+  .version('0.0.1', '-v, --version') // duplicate of the version in package.json
+  .usage('[OPTIONS]...')
+  .argument('<address>', 'The address of the owner to search for tokens.')
+  .requiredOption('-k, --apikey <key>', 'Moralis API key.')
+  .allowExcessArguments(false)
+  .parse(process.argv)
+
+console.log(program.args)
+console.log(program.opts())
